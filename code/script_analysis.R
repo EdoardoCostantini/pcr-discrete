aec5a9bf08d0ed0747d2761363f2cffd16777e11
EdoardoCostantini <38205413+EdoardoCostantini@users.noreply.github.com>
2021-06-15 17:37:08 +0200

    feature: new result plotting framework

--- code/script_analysis.R
+++ code/script_analysis.R
@@ -10,10 +10,10 @@
 # Load Results ----------------------------------------------------------
 
   inDir <- "../output/"
-  runName <- "20211406_151337"
+  runName <- "20211506_155201"
 
   # Extract last element as session info object
-  res <- readRDS(paste0(inDir, runName, "_res.rds"))
+  sim_out <- readRDS(paste0(inDir, runName, "_res.rds"))
 
   # Support Functions
   source("./helper/functions.R")
@@ -21,10 +21,64 @@
 # Analyse -----------------------------------------------------------------
 
   # Possible Results
-  names(res$results$N1000_L8_J3_P24_fl0.8_K2$rep1)
+  names(sim_out$results$N1000_L8_J3_P24_fl0.8_K2$rep1)
 
   # Mean result by name
-  lapply(res$results, average_result, result = "coefs")
-  lapply(res$results, average_result, result = "cors")
-  lapply(res$results, average_result, result = "r2")
-  lapply(res$results, average_result, result = "mses")
+  lapply(sim_out$results, average_result, result = "coefs")
+  t(sapply(sim_out$results, average_result, result = "cors"))
+  t(sapply(sim_out$results, average_result, result = "r2"))
+  t(sapply(sim_out$results, average_result, result = "mses"))
+
+# Plots -------------------------------------------------------------------
+  
+  ## Packages for plots 
+  library(ggplot2)
+  library(dplyr)
+  library(forcats)
+  library(stringr)
+
+  ## Shape data for plot
+  sim_out$conds
+  store <- as.data.frame(vector("list", 9))
+  for (i in 1:nrow(sim_out$conds)){
+    for(r in 1:sim_out$parms$dt_rep){
+      content <- data.frame(cond = sim_out$conds$tag[i],
+                            mses = as.data.frame(t(sim_out$results[[i]][[r]]$mses)),
+                            r2 = as.data.frame(t(sim_out$results[[i]][[r]]$r2))
+      )
+      store <- rbind(store, content)
+    }
+  }
+
+  gg_shape <- reshape2::melt(store,
+                             id.var = c("cond"))
+  head(gg_shape)
+
+  ## Obtain plots
+  result <- c("mses.", "r2.")[1]
+
+plot1 <- gg_shape %>%
+  filter(grepl(result, variable)) %>%
+  # Get rid of useless cond info
+  mutate(cond = fct_relabel(cond,
+                            str_replace,
+                            "^.*(?=(\\K))", "")
+  ) %>%
+  # Get better order
+  mutate(cond = fct_relevel(cond,
+                            "K10", "K7", "K5", "K3", "K2")
+  ) %>%
+  # Change labels of X axis
+  mutate(variable = fct_relabel(variable,
+                                str_replace,
+                                result, "")
+  ) %>%
+  # Main Plot
+  ggplot(aes(x = variable, y = value)) +
+  geom_boxplot() +
+  # Grid
+  facet_grid(cols = vars(cond),
+             scales = "free") +
+  # Format
+  labs(x     = NULL,
+       y     = NULL)
