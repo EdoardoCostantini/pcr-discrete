2dcde29a6bd69606cc3d7cbae0a90be010d8c706
EdoardoCostantini <38205413+EdoardoCostantini@users.noreply.github.com>
2021-06-29 14:08:50 +0200

    fix: file name month-day format correct order

--- code/settings.R
+++ code/settings.R
@@ -2,13 +2,13 @@
 ### Project:  Ordinality
 ### Author:   Edoardo Costantini
 ### Created:  2021-06-15
-### Modified: 2021-06-15
+### Modified: 2021-06-29
 
 # Empty List
 settings    <- list()
 
 # File System
-settings$start_time <- format(Sys.time(), "%Y%d%m_%H%M%S")
+settings$start_time <- format(Sys.time(), "%Y%m%d_%H%M%S")
 settings$outDir <- paste0("../output/",
                           settings$start_time,
                           "/")
