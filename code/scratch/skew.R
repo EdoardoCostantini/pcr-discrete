9070b5110a96b734922b48e086652767ff600ccf
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-10-28 10:57:56 +0200

    content: update notes on skewness

--- code/scratch/skew.R
+++ code/scratch/skew.R
@@ -1,170 +1,204 @@
+# Project:   pcr_discrete
+# Objective: Notes on generating skewed components and items
+# Author:    Edoardo Costantini
+# Created:   2022-10-28
+# Modified:  2022-10-28
+# Source:    https://github.com/trbKnl/SCaDS/blob/master/Simulation_study_4.1/generateData.R
+#            https://github.com/soogs/SCD-CovR/blob/master/functions/spcovrdata.R
 
+# Set up ----------------------------------------------------------------------
+
+# Packages
+library(sn) # for non-normal sampling
+library(MASS) # for multivariate normal sampling
+library(RegularizedSCA) # for Tucker congruence measure
 library(fungible)
 
-set.seed(20221014)
+# Fixed parameters
+I       <- 1e3  # sample size
+J       <- 9    # number of items
+VAFr    <- c(.5, .3, .2) # relative variance of each components
+VAFsum  <- 100 # total variance of the components
+CPVE    <- 0.9 # proportion of explained variance by the R components
+skewness <- c(0, -2, -2) # PC1 normal, PC2 skewed left, PC3 skewed left 
+kurtosis <- c(0, 10, 10) # adapt kurtosis to allow extreme skewness
 
-# Internals
-I    = 1e5 # sample size
-J    = 9 # number of variables
-VAFr = c(.5, .3, .2) # relative variance of each components
-VAFsum = 100 # total variance of the components
-CPVE = 0.9 # proportion of explained variance by the R components
+# Functions
+normmat <- function(X) {
+    X <- apply(X, 2, function(j) j / sqrt(sum(j^2)))
+    return(X)
+}
+
+orthmat <- function(X, verbose = FALSE) {
+    for (i in 2:ncol(X)) {
+        for (j in 1:(i - 1)) {
+            if (verbose == TRUE) {
+                print(paste0("Adjusting piar ", i, "-", j))
+            }
+            A <- X[, j]
+            b <- X[, i]
+
+            # Find projection of b on A
+            B <- as.vector(b - (t(A) %*% b / t(A) %*% A) %*% A)
+
+            # Replace in original X the orthogonalized columns
+            X[, j] <- A
+            X[, i] <- B
+        }
+    }
+    return(X)
+}
 
 # Number of components
 R <- length(VAFr)
 
-# Random sample U
-# V1: monte1 function
-# U <- monte1(
-#     seed = NULL,
-#     nsub = I,
-#     nvar = R,
-#     skewvec = c(-1, 0, 1),
-#     kurtvec = rep(0, R),
-#     cormat = diag(1, R)
-# )$data
+# Sampling --------------------------------------------------------------------
 
-# # Check skewness
-# apply(U, 2, skew)
+# Sample component scores from the SN based on Azzalini
+U <- sapply(1:R, function(j) {
+    # Collect skewness and kurtosis in a data.frame
+    cp_sk <- cbind(s = skewness, k = kurtosis)
 
-# V2: Skew normal distribution
-# Sample from the RSN based on Azzalini
-alphas <- c(10, 5, 1) # values showcased by azzalini
-U <- sapply(alphas, function(j) {
-    rsn(n = I, xi = 0, omega = 1, alpha = j, tau = 0)
+    # Define direct parameterization for the skew-t (ST) distribution
+    cpST <- c(0, 1, cp_sk[j, "s"], cp_sk[j, "k"])
+    dpST <- cp2dp(cpST, family = "ST")
+
+    # Sample from skew-t distribution
+    rst(n = I, dp = dpST)
 })
 
-# Check skewnees produced
-apply(U, 2, e1071::skewness)
-
-# Scale and continue with regulard steps
+# Scale and orthonormalize
 U <- scale(U, center = TRUE, scale = FALSE)
 U <- orthmat(U, verbose = FALSE)
 U <- normmat(U)
 
-# Random sample P
+# Random sample V
 V <- matrix(
     data = runif(J * R),
     nrow = J,
     ncol = R
 )
-V <- orthmat(V, verbose = FALSE)
-P <- normmat(V)
+
+# # Or if preferable, fixed decisions on V
+# V <- cbind(
+#     c(rep(1, 3), rep(0, 6)),
+#     rep(0, 9),
+#     c(rep(0, 6), rep(1, 3))
+# )
+# V[9, ] <- c(.001, .001, 1)
+
+# Orthonormalize V
+P <- normmat(orthmat(V, verbose = FALSE))
+
+# Or do it differently
+P <- qr.Q(qr(V))
 
 # Define D
 D <- diag(c(VAFsum * VAFr))
 
-# Create X
+# Create true X
 Xtrue <- U %*% D %*% t(P)
 
-# Scale for visualization
-Xtrue <- scale(Xtrue)
+# sample noise from a normal distribution (Ex = Error of X)
+Ex <- mvrnorm(n = I, mu = rep(0, J), Sigma = diag(J))
 
-plot(density(Xtrue[, 9]), ylim = c(0, .8))
-sapply(2:9, function(j) lines(density(Xtrue[, j])))
+# centering and scaling the Ex matrix
+Ex <- scale(Ex, center = TRUE, scale = FALSE)
 
-# Estimate 3 pcs and check how they look
-# SVD decomposition
-svd_out <- svd(Xtrue)
+# sum of squares
+ssqXtrue <- sum(Xtrue^2)
+ssqEx <- sum(Ex^2)
 
-# Compute the PC scores
-T <- (svd_out$u %*% diag(svd_out$d))
+# Compute noise scaling constant
+Escale <- sqrt(ssqXtrue * (1 - CPVE) / (CPVE * ssqEx))
 
-# Compute a vector of cumulative proportions of explained variances
-CPVE <- cumsum(prop.table(svd_out$d^2))
-CPVE
+# Add scaled noise
+X <- Xtrue + Escale * Ex
 
-# Store true components
-Tt <- U %*% D
+# Compare results --------------------------------------------------------------
+
+# > True components distributions ----------------------------------------------
+
+Ttrue <- U %*% D # compute true PCs
+par(mfrow = c(1, 3)) # set plotting device
+sapply(
+    1:R,
+    function(j) {
+        hist(
+            Ttrue[, j], # True components
+            freq = FALSE,
+            main = "Distribution of true PCs",
+            xlab = paste0("PC", j)
+        )
+    }
+)
+
+# > Estimated PCs on Xtrue -----------------------------------------------------
+
+svd_true <- svd(Xtrue)
+T_Xtrue <- svd_true$u %*% diag(svd_true$d) # components estimated on observed Xtrue
+par(mfrow = c(1, 3)) # set plotting device
+sapply(
+    1:R,
+    function(j) {
+        hist(
+            T_Xtrue[, j], # Estimated components on True
+            freq = FALSE,
+            main = "Distribution of estimated PCs on X (true)",
+            xlab = paste0("PC", j)
+        )
+    }
+)
+
+# > Estimated PCs on X ---------------------------------------------------------
+
+svd_obs <- svd(scale(X))
+T_X <- svd_obs$u %*% diag(svd_obs$d) # components estiamted on observed X
+par(mfrow = c(1, 3)) # set plotting device
+sapply(
+    1:R,
+    function(j) {
+        hist(
+            T_X[, j], # Estimated components on True
+            freq = FALSE,
+            main = "Distribution of estimated PCs on X (obs)",
+            xlab = paste0("PC", j)
+        )
+    }
+)
+
+# > True X distributions -------------------------------------------------------
+
+par(mfrow = c(3, 3))
+sapply(
+    1:J,
+    function(j) {
+        hist(
+            Xtrue[, j], # True X
+            freq = FALSE,
+            main = "Distribution of X (true)",
+            xlab = paste0("x", j)
+        )
+    }
+)
+
+# > Observed X distributions ---------------------------------------------------
+
+par(mfrow = c(3, 3))
+sapply(
+    1:J,
+    function(j) {
+        hist(
+            X[, j], # True X
+            freq = FALSE,
+            main = "Distribution of X (obs)",
+            xlab = paste0("x", j)
+        )
+    }
+)
+
+# > Check correlations between matrices ----------------------------------------
 
 # Check correlations with true and untrue components
-cor(Tt[, 1], T[, 1])
-cor(Tt[, 2], T[, 2])
-cor(Tt[, 3], T[, 3])
-
-# Plot true components
-plot(density(Tt[, 1]), ylim = c(0, 25))
-sapply(2:R, function(j) lines(density(Tt[, j])))
-
-# Is this good recovery?
-plot(density(T[, 1]), ylim = c(0, 1.5))
-sapply(2:R, function(j) lines(density(T[, j])))
-
-x <- fGarch::rsnorm(1e4, mean = 0, sd = 1, xi = 10)
-skew(x)
-plot(density(fGarch::rsnorm(1e4, mean = 0, sd = 1, xi = 1.5))
-
-# Check resulting item skewness -----------------------------------------------
-
-    set.seed(1235)
-
-    # Generate data
-    XTP <- generateXTP(
-        I = 1e4,
-        J = parms$P,
-        VAFr = parms$XTP_VAFr,
-        VAFsum = parms$XTP_VAFsum,
-        CPVE = .9,
-        skewness = c(0, -2, 2),
-        kurtosis = c(0, 10, 10)
-    )
-
-    # Store items skewness results
-    apply(XTP$T, 2, e1071::skewness)
-    svd_out <- svd(XTP$X)
-    T <- (svd_out$u %*% diag(svd_out$d))
-    apply(T[, 1:3], 2, e1071::skewness)
-    sort(apply(XTP$X, 2, e1071::skewness))
-    order(XTP$skew)
-
-    # Graphs
-    par(mfrow = c(1, 3))
-    apply(XTP$T, 2, function(x) plot(density(x)))
-
-    par(mfrow = c(3, 4))
-    apply(XTP$X[, order(XTP$skew)], 2, function(x) plot(density(x)))
-
-# Overcome skewed normal distribution limit of gamma < 1 ----------------------
-
-# The SKewed normal has the limit of gamma < 1
-
-# This works
-cp <- c(0, 1, .8)
-dp <- cp2dp(cp, family = "SN")
-x <- rsn(1e5, dp = dp)
-e1071::skewness(x)
-e1071::kurtosis(x)
-
-# But this does not
-cp <- c(0, 1, 2)
-dp <- cp2dp(cp, family = "SN")
-
-# So we move to the Skewed t distribution which allows changing the excess kurtosis
-cpST <- c(0, 1, 2, 10)
-dpST <- cp2dp(cpST, family = "ST")
-x <- rst(1e6, dp = dpST)
-plot(density(x))
-e1071::skewness(x)
-e1071::kurtosis(x)
-
-# Note a normal distribution with kurtosis
-cpST <- c(0, 1, 0, 0)
-dpST <- cp2dp(cpST, family = "ST")
-x <- rst(1e5, dp = dpST)
-e1071::skewness(x)
-e1071::kurtosis(x)
-
-z <- rnorm(1e5)
-e1071::skewness(z)
-e1071::kurtosis(z)
-
-plot(density(x))
-lines(density(rnorm(1e4)))
-
-# Push it even more
-cpST <- c(0, 1, 3, 40)
-dpST <- cp2dp(cpST, family = "ST")
-x <- rst(1e6, dp = dpST)
-plot(density(y2))
-e1071::skewness(x)
-e1071::kurtosis(x)
+TuckerCoef(Ttrue, T_X)$tucker_vector
+TuckerCoef(Ttrue, T_Xtrue)$tucker_vector
