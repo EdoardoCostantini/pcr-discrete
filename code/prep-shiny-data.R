c5840c58ab8fbb703114aff3f7c5d977ced81d2a
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-02-22 11:40:55 +0100

    hk: fix factor levels of variables for exporting data

--- code/prep-shiny-data.R
+++ code/prep-shiny-data.R
@@ -2,16 +2,29 @@
 # Objective: pre-process input data for actual use in shiny app
 # Author:    Edoardo Costantini
 # Created:   2023-02-21
-# Modified:  2023-02-21
+# Modified:  2023-02-22
 # Notes:     This script prepares the input data for the shiny app plotpcrnotnormal.
 
 # Read output
 inDir <- "../output/"
 grep("_box", list.files(inDir), value = TRUE)
-resPCRDIS <- readRDS(paste0(inDir, "20220421_154258_box.rds"))
+resPCRdis <- readRDS(paste0(inDir, "20220421_154258_box.rds"))
 
 # Split variable into two useful columns
-resPCRDIS[c("outcome", "method")] <- str_split_fixed(gg_shape$variable, "\\.", 2)
+resPCRdis[c("outcome", "method")] <- stringr::str_split_fixed(resPCRdis$variable, "\\.", 2)
+
+# Define a factor with desired order of the methods
+resPCRdis$method <- factor(
+    resPCRdis$method,
+    levels = unique(resPCRdis$method)
+)
+
+# Express proportion of discretized variables as a ratio
+resPCRdis$D <- factor(
+    resPCRdis$D,
+    levels = unique(resPCRdis$D)[c(2, 3, 1)],
+    labels = c("1/3", "2/3", "1")
+)
 
 # Save the two objects as .rda ready for shiny app
-save(resPCRDIS, file = "../output/resPCRDIS.rda")
+save(resPCRdis, file = "../output/resPCRdis.rda")
